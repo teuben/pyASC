59bf8de0d98c44ed4f4c366c3047bfecc258432c
Peter Teuben <teuben@gmail.com>
2016-11-03 13:25:14 -0400

    separare class

---
+++ ASC/Dtime.py
@@ -0,0 +1,77 @@
+#! /usr/bin/env python
+
+
+import time
+import numpy as np
+import logging
+
+#  For some variations on this theme, e.g.  time.time vs. time.clock, see
+#  http://stackoverflow.com/questions/7370801/measure-time-elapsed-in-python
+#
+
+class Dtime(object):
+    """ Class to help measuring the wall clock time between tagged events
+        Typical usage:
+        dt = Dtime()
+        ...
+        dt.tag('a')
+        ...import logging 
+        dt.tag('b')
+        dt.end()
+    """
+    def __init__(self, label=".", report=True):
+        self.start = self.time()
+        self.init = self.start
+        self.label = label
+        self.report = report
+        self.dtimes = []
+        dt = self.init - self.init
+        if self.report:
+            #logging.info("Dtime: %s ADMIT " % (self.label + self.start)) #here took out a '
+            logging.info("Dtime: %s BEGIN " % self.label + str(dt))
+
+    def reset(self, report=True):
+        self.start = self.time()
+        self.report = report
+        self.dtimes = []
+
+    def tag(self, mytag):
+        t0 = self.start
+        t1 = self.time()
+        dt = t1 - t0
+        self.dtimes.append((mytag, dt))
+        self.start = t1
+        if self.report:
+            logging.info("Dtime: %s " % self.label + mytag + "  " + str(dt))
+        return dt
+
+    def show(self):
+        if self.report:
+            for r in self.dtimes:
+                logging.info("Dtime: %s " % self.label + str(r[0]) + "  " + 
+                    str(r[1]))
+        return self.dtimes
+
+    def end(self):
+        t0 = self.init
+        t1 = self.time()
+        dt = t1 - t0
+        if self.report:
+            logging.info("Dtime: %s END " % self.label + str(dt))
+        return dt
+
+    def time(self):
+        """ pick the actual OS routine that returns some kind of timer
+        time.time   :    wall clock time (include I/O and multitasking overhead)
+        time.clock  :    cpu clock time
+        """
+        return np.array([time.clock(), time.time()])
+
+if __name__ == '__main__':
+    logging.basicConfig(level = logging.INFO)
+    dt = Dtime("testingDtime")
+    dt.tag('one')
+    print("Hello Dtime World")
+    dt.tag('two')
+    dt.end()
+       
