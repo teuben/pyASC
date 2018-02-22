b6c347b4a1621e79c566aa4e43d290ce3ab13957
jsc6121 <jsc6121@gmail.com>
2018-02-22 14:38:30 -0500

    Added summary function, deletes folders of non-detected fits files

--- examples/try_astride.py
+++ examples/try_astride.py
@@ -8,16 +8,31 @@ from __future__ import print_function
 from astride import Streak
 import glob
 import sys
+import shutil
 
 def do_dir(d):
     """
     process a directory 'd'
     """
+    num = 0
+    detected = 0
+    fileCount = 0
+    zero = 0
     ffs = glob.glob(d+'/*.FIT*')     # results in a non-numeric order
+    f = open(d+'/summary.txt','w')  #Creates summary text file 
+    f.write('Streaks found in files: \n')   #Creates first line for summary file
     for ff in ffs:
         # print(ff)
-        do_one(ff,ff[:ff.rfind('.')])
-
+        num = do_one(ff,ff[:ff.rfind('.')])
+        if num == 0:
+            zero += 1
+        else:
+            detected += int(num)    #Counter of how many streaks detected
+            f.write(ff + '\n') 
+        fileCount += 1   #Counter for how many files analyzed         
+    # Produce and write summary file 
+    f.write('... \n' 'Files analyzed: ' + str(fileCount)+ '\n' + 'Streaks detected: ' + str(detected) \
+    + '\n' + 'Files with no detections: ' + str(zero))
 def do_one(ff,output_path=None):
     """
     process a directory one fits-file (ff)
@@ -45,6 +60,14 @@ def do_one(ff,output_path=None):
     else:
         sys.stdout.write('*')
     sys.stdout.flush()
+    
+    #Delete/move files
+    if n == 0:
+        shutil.rmtree(output_path)
+    
+    return int(n)
+    
+    
 #do_one('20151108_MD01_raw/IMG00681.FIT')
 #do_dir('20151108_MD01_raw')
 
