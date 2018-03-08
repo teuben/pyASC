ec213d7431c462af76df40e8ee165c3ef092bc97
jsc6121 <jsc6121@gmail.com>
2018-03-08 16:13:27 -0500

    Organized coding structure

--- examples/try_astride.py
+++ examples/try_astride.py
@@ -12,20 +12,46 @@ import shutil
 import os
 import tkinter as tk
 
-def do_dir(d,dsum=None):
-    """
-    process a directory 'd'
-    """
-    #print("Outputting in directory: " + dsum)
-    if dsum == None:
-        dsum = d
+def get_arg(argv):
+    if len(argv) == 1: 
+        return get_int_arg(argv)
     else:
-        if not os.path.exists(dsum):    
-            os.mkdir(dsum)
-    num = 0
-    detected = 0
-    fileCount = 0
-    zero = 0
+        return get_cmd_arg(argv)
+ 
+def get_cmd_arg(argv):
+    import argparse as ap
+    print(argv)
+    parser = ap.ArgumentParser()
+    parser.add_argument('-c','--contour',nargs=1,help = 'blah Control value')
+    args=vars(parser.parse_args())
+    print(args)
+    
+    if args['contour'] != None: contour = float(args['contour'][0])
+    print(contour)
+    file_pathin = 'test'
+    file_pathout = 'junk'
+    shape = .14
+    area = 120
+    
+    return (file_pathin,file_pathout,shape,area,contour)
+    
+def get_int_arg(argv):
+    #Creates folder input browsers
+    winin = tk.Tk()
+    winin.withdraw()
+    winin.attributes('-topmost', True)
+    file_pathin = filedialog.askdirectory(title = "Select input")
+    
+    #Creates folder output browsers   
+    winout = tk.Tk()
+    winout.withdraw()
+    winout.attributes('-topmost', True)
+    file_pathout = filedialog.askdirectory(title = "Select output")
+    
+    winout.destroy()
+    winin.destroy()
+    
+    #ask user for remaining arguments
     print("\nClicking enter will apply default values, entering a value will change it.")
     nshape = input("Shape value (1=circle, .1=thin oval) (default = 0.14): ")
     if nshape == "":
@@ -44,6 +70,23 @@ def do_dir(d,dsum=None):
         contour = 12
     else:
         contour = float(ncontour)
+        
+    return(file_pathin,file_pathout,shape,area,contour)
+    
+def do_dir(d,dsum=None,shape=.14,area=120,contour=12):
+    """
+    process a directory 'd'
+    """
+    #print("Outputting in directory: " + dsum)
+    if dsum == None:
+        dsum = d
+    else:
+        if not os.path.exists(dsum):    
+            os.mkdir(dsum)
+    num = 0
+    detected = 0
+    fileCount = 0
+    zero = 0
     
     ffs = glob.glob(d+'/*.FIT*')     # results in a non-numeric order
     ffs.sort()                       # on linux wasn't sorted, on dos it was
@@ -111,24 +154,11 @@ def do_one(ff,output_path=None,shape=None,area=None,contour=None):
 #do_dir('20151108_MD01_raw')
 
 if __name__ == '__main__':    
-    #Creates folder input browsers
-    winin = tk.Tk()
-    winin.withdraw()
-    winin.attributes('-topmost', True)
-    file_pathin = filedialog.askdirectory(title = "Select input")
-    
-    #Creates folder output browsers   
-    winout = tk.Tk()
-    winout.withdraw()
-    winout.attributes('-topmost', True)
-    file_pathout = filedialog.askdirectory(title = "Select output")
-    
-    winout.destroy()
-    winin.destroy()
+    (file_pathin,file_pathout,shape,area,contour) = get_arg(sys.argv)
     #Prints selected folders
     print("Running in data directory %s" % file_pathin)
     print("Outputting in data directory %s" % file_pathout)
-    do_dir(file_pathin,file_pathout)
+    do_dir(file_pathin,file_pathout,shape,area,contour)
     
     
     #print("Running in data directory %s" % sys.argv[1])
