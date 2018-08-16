ae63160175797a3538d5d5416656423592b0c95f
Peter Teuben <teuben@gmail.com>
2018-08-16 08:32:25 -0400

    handle jpg as well as fits only paths

--- examples/fitsdiff2.py
+++ examples/fitsdiff2.py
@@ -12,7 +12,6 @@ import glob
 import sys
 import shutil
 import os
-from astropy.io import fits
 import numpy as np
 
 
@@ -21,13 +20,36 @@ if len(sys.argv) == 3:
     f2 = sys.argv[2]
     print("Using %s %s" % (f1,f2))
 
-hdu1 = fits.open(f1)
-hdu2 = fits.open(f2)
+#  check file extension
 
-h2 = hdu2[0].header
 
-d1 = hdu1[0].data.astype(np.float32)
-d2 = hdu2[0].data.astype(np.float32)
+if fn[fn.rfind('jpg'):] == 'jpg':
+    # 
+    use_fits = False
+else:
+    # assume it's fits
+    use_fits = True
+
+
+
+if use_fits:
+    from astropy.io import fits
+
+    hdu1 = fits.open(f1)
+    hdu2 = fits.open(f2)
+
+    h2 = hdu2[0].header
+
+    d1 = hdu1[0].data.astype(np.float32)
+    d2 = hdu2[0].data.astype(np.float32)
+else:
+    from PIL import Image
+
+    im1 = Image.open(f1)
+    im2 = Image.open(f2)
+
+    d1 = np.asarray(im1)
+    d2 = np.asarray(im2)
 
 print(f1,d1.min(),d1.max())
 print(f2,d2.min(),d2.max())
@@ -42,7 +64,7 @@ fidelity = max1 / std1
 
 print("MEAN/STD/FID:",diff.mean(), std1, fidelity)
 
-if False:
+if use_fits:
     fits.writeto('diff.fits',diff,h2,overwrite=True)
 #
 #fid  = np.abs(d2) / np.max(np.abs(diff),std1/1.4)
