57fb13089ef967b1c6c1f787653afed4f135e02b
zack Rabin <zack@zacks-MBP.fios-router.home>
2019-04-21 14:38:29 -0400

    update

--- examples/summary_parser.py
+++ examples/summary_parser.py
@@ -1,3 +1,4 @@
+#! /usr/bin/env python
 import argparse as ap
 import glob
 import datetime
@@ -5,6 +6,7 @@ import datetime
 
 
 if __name__ == "__main__":
+    path = '[0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9] to [0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]'
     parser = ap.ArgumentParser()
     parser.add_argument('-i','--filein', nargs=1, help = 'Directory of outputs of try_astride.py')
 
@@ -16,9 +18,7 @@ if __name__ == "__main__":
         # example directory match "2019-12-02 to 2019-12-03"
         list_dir = glob.glob(path)
         #module called filepath that can use instead of glob.glob
-        file_path = max(list_dir, key = lambda f:datetime.date(int(f[0:4]),int(f[5:7]),int(f[8:10])))
-        
-        
+        file_path = max(list_dir, key = lambda f:datetime.date(int(f[0:4]),int(f[5:7]),int(f[8:10]))) 
         
     summary = open(file_path+"/summary.txt", 'r')
 
