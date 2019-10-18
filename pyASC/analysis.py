0d18279056eaba31c5e85d532afcdbd066b55685
Geoff Ryan <geoffrey.s.ryan@gmail.com>
2019-10-18 01:57:01 -0400

    makeNightlyMovie works

--- pyASC/analysis.py
+++ pyASC/analysis.py
@@ -1,20 +1,26 @@
+import numpy as np
 import cv2
 import astropy.io.fits
+# import matplotlib.pyplot as plt
 
 
 def makeNightlyMovie(arch, node, date, outname):
 
-    width, height = 100, 100
+    width, height = 1392, 1040
     size = (width, height)
-    out = cv2.VideoWriter(outname, cv2.VideoWriter_fourcc(*'mp4v'), 15, size)
+    out = cv2.VideoWriter(outname, cv2.VideoWriter_fourcc(*'mp4v'), 10.0, size)
 
-    for filename in arch.getFITS(node, date):
+    frameData = np.empty((height, width, 3), dtype=np.uint8)
+
+    for i, filename in enumerate(arch.getFITS(node, date)):
         # process filename (FITS file) into img_array
-        hdul = astropy.io.fits.open(filename)
-        print(hdul.info())
-        hdul.close()
-        # out.write(f.data)
-        break
-    out.release()
+        print(i, filename)
+        with astropy.io.fits.open(filename) as hdul:
+            hdu = hdul[0]
+            data = hdu.data/256
+            frameData[:, :, 0] = data
+            frameData[:, :, 1] = data
+            frameData[:, :, 2] = data
+        out.write(frameData)
 
-    return
+    out.release()
