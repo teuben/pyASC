00188910ab8ebc74f89fac6f2bad7d7c9f3d94a3
Peter Teuben <teuben@gmail.com>
2020-06-27 23:32:12 -0400

    deal with other timestamping

--- ASC/SkyStats.py
+++ ASC/SkyStats.py
@@ -16,7 +16,14 @@ if __name__ == '__main__':
         nx = d.shape[1]
         ny = d.shape[0]
         dc = d[ny//2-box:ny//2+box, nx//2-box:nx//2+box]
-        hms = h['TIME-OBS'].split(':')
+        if 'TIME-OBS' in h:
+            # 
+            hms = h['TIME-OBS'].split(':')
+        elif 'DATE-LOC' in h:
+            # '2020-02-29T20:13:34.920'
+            hms = h['DATE-LOC'].split('T')[1].split(':')
+        else:
+            hms = -999.999
         t = float(hms[0]) + float(hms[1])/60  + float(hms[2])/3600
         
         print("%.4f %g" % (t,np.median(dc)))
