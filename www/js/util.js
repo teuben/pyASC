996fb4b5462a835f60a40674830bd38047d88995
Jonathan Lin <nthanjolin@gmail.com>
2021-08-20 03:33:39 -0400

    dynamically display tagged meteors

--- www/js/util.js
+++ www/js/util.js
@@ -5,14 +5,23 @@ const POOR_LIM = MAX_VAL * (1/3);
 const MEDIUM_LIM = MAX_VAL * (2/3);
 const GOOD_LIM = MAX_VAL * (3/3);
 
+function filenameToDate(name, asMoment) {
+    let m = name.match(FILE_REGEX).slice(1);
+    let isoDate = `${m[0]}-${m[1]}-${m[2]}T${m[3]}:${m[4]}:${m[5]}${m[6] ? `,${m[6]}${m[7]}` : ''}`;
+    let isUTC = name.indexOf('Z') !== -1;
+    let date = (isUTC ? moment.utc(isoDate) : moment(isoDate)).utc();
+    return asMoment ? date : date.toDate();
+}
+
+function dateToPath(date) {
+    console.log(dateToPath);
+}
+
 function updateSkymap(currentFile) {
     let latitude = 39.0021;
     let longitude = -76.956;
     
-    let m = currentFile.match(FILE_REGEX).slice(1);
-    let isoDate = `${m[0]}-${m[1]}-${m[2]}T${m[3]}:${m[4]}:${m[5]}${m[6] ? `,${m[6]}${m[7]}` : ''}`;
-    let isUTC = currentFile.indexOf('Z') !== -1;
-    let date = (isUTC ? moment.utc(isoDate) : moment(isoDate)).utc().toDate();
+    let date = filenameToDate(currentFile);
 
     $('#filetime').text(date.toString());
     console.log(date);
--- www/tagger/regions.php
+++ www/tagger/regions.php
@@ -16,27 +16,44 @@
         die(json_encode($output));
     }
 
-    if (!isset($_GET['path'])) die('No path specified.');
-    if ($_GET['action'] == 'list') {
-        $stmt = $db->prepare('SELECT * FROM regions WHERE path=:path');
-        $stmt->bindValue(':path', $_GET['path'], SQLITE3_TEXT);
+    if ($_GET['action'] == 'tag') {
+        if (!isset($_GET['tag'])) die('No search tag specified.');
+        $output = array();
+        $stmt = $db->prepare("SELECT path, params FROM regions WHERE tags LIKE '%' || :tag || '%'");
+        $stmt->bindValue(':tag', $_GET['tag'], SQLITE3_TEXT);
         $result = $stmt->execute();
-        if (!$result) {
-            die($db->lastErrorMsg());
+        while ($row = $result->fetchArray(SQLITE3_ASSOC)) {
+            array_push($output, $row);
         }
-        echo ($output = $result->fetchArray(SQLITE3_ASSOC)) ? json_encode($output) : '{}';
-    } else if ($_GET['action'] == 'update') {
-        if (!isset($_GET['tags'])) die('No tags specified.');
-        if (!isset($_GET['params'])) die('No params specified.');
-        $stmt = $db->prepare('INSERT OR REPLACE INTO regions (path, tags, params) VALUES (:path, :tags, :params)');
-        $stmt->bindValue(':path', $_GET['path'], SQLITE3_TEXT);
-        $stmt->bindValue(':tags', $_GET['tags'], SQLITE3_TEXT);
-        $stmt->bindValue(':params', $_GET['params'], SQLITE3_TEXT);
-        if (!$stmt->execute()) {
-            die($db->lastErrorMsg());
-        }
-    } else {
-        die('No valid action.');
+        die(json_encode($output));
+    }
+
+    if (!isset($_GET['path'])) die('No path specified.');
+
+    switch($_GET['action']) {
+        case 'list':
+            $stmt = $db->prepare('SELECT * FROM regions WHERE path=:path');
+            $stmt->bindValue(':path', $_GET['path'], SQLITE3_TEXT);
+            $result = $stmt->execute();
+            if (!$result) {
+                die($db->lastErrorMsg());
+            }
+            echo ($output = $result->fetchArray(SQLITE3_ASSOC)) ? json_encode($output) : '{}';
+            break;
+        case 'update':
+            if (!isset($_GET['tags'])) die('No tags specified.');
+            if (!isset($_GET['params'])) die('No params specified.');
+            $stmt = $db->prepare('INSERT OR REPLACE INTO regions (path, tags, params) VALUES (:path, :tags, :params)');
+            $stmt->bindValue(':path', $_GET['path'], SQLITE3_TEXT);
+            $stmt->bindValue(':tags', $_GET['tags'], SQLITE3_TEXT);
+            $stmt->bindValue(':params', $_GET['params'], SQLITE3_TEXT);
+            if (!$stmt->execute()) {
+                die($db->lastErrorMsg());
+            }
+            break;
+        default:
+            die('No valid action.');
+            break;
     }
 
     $db->close();
